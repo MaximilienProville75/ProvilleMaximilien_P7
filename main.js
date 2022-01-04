9b2130f4c50ac8f5b6f39ae6010d3149d188aed1
PROVILLE Maximilien <maximilien.proville77@gmail.com>
2022-01-04 23:51:28 +0100

    SearchBar Worling

--- main.js
+++ main.js
@@ -1,12 +1,22 @@
 const recipeList = document.getElementById("recipeList");
 const searchBar = document.getElementById("searchBar");
+let recipes = [];
+
+searchBar.addEventListener("keyup", (e) => {
+  const searchString = e.target.value.toLowerCase();
+
+  const filteredRecipesName = recipes.filter((character) => {
+    return character.name.toLowerCase().includes(searchString);
+  });
+  displayRecipes(filteredRecipesName);
+});
 
 const loadRecipes = async () => {
   try {
     const res = await fetch("/recipes.json");
-    let recipes = await res.json();
+    recipes = await res.json();
     displayRecipes(recipes);
-    console.log(recipes);
+    // console.log(recipes);
   } catch (err) {
     console.log(err);
   }
@@ -17,6 +27,7 @@ const displayRecipes = (recipes) => {
 
     .map((recipe) => {
       const recipeIngredient = recipe.ingredients;
+
       return `
     <li class="Recipes">
       <img src=" "  class="RecipesImg"/>
@@ -32,10 +43,13 @@ const displayRecipes = (recipes) => {
               <ul class="IngredientUl">
                 ${recipeIngredient.map(
                   (recipe) =>
-                    `<li>${recipe.ingredient} ${recipe.quantity} ${recipe.unit}</li>`
+                    `<li>${recipe.ingredient} ${recipe.quantity} ${
+                      recipe.unit === undefined ? "" : recipe.unit
+                    }</li>`
                 )}
               </ul>
             </div>
+            <p>${recipe.description}</p>
           </div>
         </div>
     </li>
